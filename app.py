160a32bceeeb56fe9676b88c55956fd00ce13809
Johnson-07x <Johnson-07x@gmail.com>
2024-05-12 23:20:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import qrcode
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.form['text']
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(data)
+    qr.make(fit=True)
+    qr_img = qr.make_image(fill='black', back_color='white')
+
+    qr_img.save('static/qrcode.png')
+    return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
